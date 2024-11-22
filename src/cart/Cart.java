af51b3d81034b1f0bd86f162ec40f9f59db95abb
legoChoi <chlx159@gmail.com>
2024-11-22 11:15:13 +0900

    feat: lv04 장바구니 추가 메소드 + 오더 메뉴 리스트 콘솔에 띄워주기 및 분기 추가

--- src/cart/Cart.java
+++ src/cart/Cart.java
@@ -3,6 +3,8 @@ package cart;
 import menuItems.MenuItem;
 
 public interface Cart {
+    boolean getState();
+    void setState(boolean state);
     void add(MenuItem menuItem);
     void remove(MenuItem menuItem);
 }
--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -44,13 +44,19 @@ public class Kiosk {
 
     /** 메인 메뉴 보여주는 메소드 **/
     private void showMainMenu() {
-        String title = "\n[ MAIN MENU ]\n" +
+        String mainMenu = "\n[ MAIN MENU ]\n" +
                 "1. Burgers\n" +
                 "2. Drinks\n" +
                 "3. Desserts\n" +
-                String.format("%-13s | %s", "0. 종료", "종료");
+                String.format("0. %-13s | %s", "종료", "종료");
 
-        consoleOutput.print(title);
+        consoleOutput.print(mainMenu);
+
+        String orderMenu = "\n[ ORDER MENU ]\n" +
+                String.format("4. %-13s | %s\n", "Orders", "장바구니를 확인 후 주문합니다.") +
+                String.format("5. %-13s | %s", "Cancel", "진행중인 주문을 취소합니다.");
+
+        consoleOutput.print(orderMenu);
     }
 
     public void start() {
@@ -80,8 +86,7 @@ public class Kiosk {
             return false;
         }
         else if (commandInput < 0 || commandInput > 3) {
-            consoleOutput.print(ExceptionMessages.NotValidMenuInput.getMessage());
-            return true;
+            throw new NotValidInputException();
         }
         else if (commandInput == 1) {
             showMenu(burgerMenu);
@@ -92,6 +97,14 @@ public class Kiosk {
             return true;
         }
         else if (commandInput == 3) { // 디저트 메뉴는 아직 없음
+            return true;
+        }
+        else if (commandInput == 4) {
+            // 장바구니 확인 후 주문 로직
+            return true;
+        }
+        else if (commandInput == 5) {
+            // 장바구니 확인 후 취소 로직
             return true;
         }
 
@@ -130,6 +143,8 @@ public class Kiosk {
                         menuItem.getDescription()
                 ));
 
+                showCartMenu(menuItem);
+
                 menu.setState(false);
             } catch (NotValidInputException e) {
                 consoleOutput.print(e.getMessage());
@@ -138,5 +153,41 @@ public class Kiosk {
         }
 
         menu.setState(true);
+    }
+
+    private void showCartMenu(MenuItem menuItem) {
+        int commandInput;
+
+        while (cart.getState()) {
+            consoleOutput.print("\n위 메뉴를 장바구니에 추가하시겠습니까?");
+            consoleOutput.print(String.format(
+                    "1. %-15s 2. %s",
+                    "확인",
+                    "취소"
+            ));
+
+            try {
+                commandInput = consoleInput.getIntInput();
+
+                if (commandInput == 1) {
+                    cart.add(menuItem);
+                    break;
+                }
+                if (commandInput == 2) {
+                    break;
+                }
+
+                if (commandInput <= 0 || commandInput > 2) {
+                    throw new NotValidInputException();
+                }
+
+                cart.setState(false);
+            } catch (NotValidInputException e) {
+                consoleOutput.print(e.getMessage());
+                consoleInput.getStringInput(); // 개행 문자 처리
+            }
+        }
+
+        cart.setState(true);
     }
 }
